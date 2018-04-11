73345a3b383b69ad6a4dc3ad2ca74c6025d17a9d
tragic86 <oneil5@aol.com>
2018-04-11 01:37:21 +0000

    print different combos

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(0, 9):
+   for decimal in range(number + 1, 10):
+    if number < 8 and decimal < 9:
+       print('{:d}{:d},'.format(number, decimal), end=" ")
+    else:
+       print('{:d}{:d}'.format(number, decimal))
