1100a845428f395dbe896a82543d48f2eabba7a1
tragic86 <oneil5@aol.com>
2018-04-13 05:56:24 +0000

    moving indent changed code so had to change up code to get correct output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ from sys import argv
 def print_args(argv):
     if len(argv) == 1:
         print("{} arguments.".format(len(argv) - 1))
-    elif len(argv) == 2:
+    elif len(argv) < 2:
         print("{} argument:".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
