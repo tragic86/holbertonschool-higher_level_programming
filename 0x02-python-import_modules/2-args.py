c333dc3d14d79954c8b952600d21078c296f1a04
tragic86 <oneil5@aol.com>
2018-04-15 04:01:19 +0000

    fix indent

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def print_args(argv):
-    if len(argv) == 1:
-        print("{} arguments.".format(len(argv) - 1))
-    elif len(argv) < 2:
-        print("{} argument:".format(len(argv) - 1))
-    else:
-        print("{} arguments:".format(len(argv) - 1))
-
-        for i, value in enumerate(argv[1:], 1):
-            print("{}: {}".format(i, value))
-
 
 if __name__ == "__main__":
-    import sys
-    print_args(sys.argv)
+    from sys import argv
+
+
+
+    if len(argv) == 1:
+        print('0 arguments.')
+
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{:d}: {:s}".format(i, arg))
+
+
+
+
