b5b53be9f0f3eeaed78ce600ec19eedaf8c4b617
tragic86 <oneil5@aol.com>
2018-04-16 03:58:22 +0000

    pep8 fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,6 @@
 if __name__ == "__main__":
     from sys import argv
 
-
-
     if len(argv) == 1:
         print('0 arguments.')
 
@@ -16,7 +14,3 @@ if __name__ == "__main__":
 
     for i, arg in enumerate(argv[1:], start=1):
         print("{:d}: {:s}".format(i, arg))
-
-
-
-
