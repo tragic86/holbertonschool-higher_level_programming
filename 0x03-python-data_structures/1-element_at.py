4fd393f225cf65dacb8f9f2777cbf8352fbabbb8
tragic86 <oneil5@aol.com>
2018-04-17 20:24:50 +0000

    retrive an element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+
+    if idx < 0:
+        return none
+
+    elif idx > len(my_list):
+        return none
+
+    else:
+        return my_list[idx]
