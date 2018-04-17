58b1ffb34ed015bdf22682306310d85c2611bb15
tragic86 <oneil5@aol.com>
2018-04-17 20:47:58 +0000

    added loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 
-    if idx < 0:
-        return None
+    for i in (my_list):
 
-    elif idx > len(my_list):
-        return None
+        if idx < 0:
+            return None
 
-    else:
-        return my_list[idx]
+        elif idx > len(my_list):
+            return None
+
+        else:
+            return my_list[idx]
