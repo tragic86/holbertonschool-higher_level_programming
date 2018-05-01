c71620a0d8505b25262f961e832f6a3eb63790f1
tragic86 <oneil5@aol.com>
2018-05-01 06:33:54 +0000

    created variable to hold return

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,12 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    intger = 0
     for i in range(x):
         try:
             print('{:d}'.format(my_list[i]), end='')
+            intger += 1
         except (ValueError, TypeError):
             continue
-    i += 1
     print()
-    return(i)
+    return (intger)
