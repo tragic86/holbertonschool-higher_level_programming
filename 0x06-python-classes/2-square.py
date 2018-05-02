d4dd2390728c8fa37159be1ac92689b0eba1b523
tragic86 <oneil5@aol.com>
2018-05-02 06:21:51 +0000

    square with value error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """empty square"""
+
+    def __init__(self, size=0):
+        """
+        Args:
+            param1 (size): size of square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
