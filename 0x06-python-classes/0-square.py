cfa9b6f7b4426d9c5addce54c43013aeb6bce644
tragic86 <oneil5@aol.com>
2018-05-02 06:26:38 +0000

    empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """empty square"""
+
+    def square(self):
+        """defined square
+
+
+        Args:
+             None
+
+        Return:
+               None
+
+        """
+        p = Square()
+        """update value
+        will use p.square to
+        print
+        """
+
+        p.square()
+        """print square
+
+        will print empty
+        square
+        """
