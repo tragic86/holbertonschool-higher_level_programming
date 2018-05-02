162f40c3b804c664f9577088341010cdcc467a90
tragic86 <oneil5@aol.com>
2018-05-02 06:24:47 +0000

    square with priviate class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """empty square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        """
+        Args:
+            param1 (size): size of square
+
+        """
