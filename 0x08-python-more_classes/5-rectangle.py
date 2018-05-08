690c4dd77351657f9073bb9e8848c8f0b5f40de0
tragic86 <oneil5@aol.com>
2018-05-08 03:18:51 +0000

    delete rectangle

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+function for rectangle
+"""
+
+
+class Rectangle():
+    """define rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+            if self.width == 0 or self.height == 0:
+                return(" ")
+            return('\n'.join(("#" * self.width) for i in range(self.height)))
+
+    def __repr__(self):
+
+        return('Rectangle({:d}, {:d})'.format(self.width, self.height))
+
+    def __del__(self):
+        print('Bye rectangle...')
