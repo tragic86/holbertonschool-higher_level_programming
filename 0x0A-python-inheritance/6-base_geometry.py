8144a9dba476c9efebd6001610a22333367b3db8
tragic86 <oneil5@aol.com>
2018-05-16 06:01:08 +0000

    get area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Create a class
+"""
+
+
+class BaseGeometry:
+    """Create class"""
+
+    def area(self):
+        """
+        raise error
+        """
+        raise Exception("area() is not implemented")
