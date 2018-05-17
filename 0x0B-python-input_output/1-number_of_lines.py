08f260f700b80fc54bc641a5889d7436de8f9f87
tragic86 <oneil5@aol.com>
2018-05-17 05:45:41 +0000

    added counter

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,10 +3,11 @@
 count numbers of lines
 """
 
+
 def number_of_lines(filename=""):
 
-
-   with open(filename, encoding='UTF-8') as f:
-      for line_count, words in enumerate(f, 1):
-         pass
-      return line_count
+    line_count = 0
+    with open(filename, encoding='UTF-8') as f:
+        for i in f:
+            line_count += 1
+        return line_count
