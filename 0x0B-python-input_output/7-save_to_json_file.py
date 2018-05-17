5a80fce505e6bdebc76c5fb34a54a68b82f7b4e4
tragic86 <oneil5@aol.com>
2018-05-17 06:01:00 +0000

    save file

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+
+def save_to_json_file(my_obj, filename):
+
+    with open(filename, mode='w', encoding='UTF-8') as f:
+        return f.write(json.dumps(my_obj))
