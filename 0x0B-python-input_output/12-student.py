39b5cbc04d4f9fe27e54d937a63943d6007e155d
tragic86 <oneil5@aol.com>
2018-05-17 06:25:10 +0000

    added comment

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-
+"""
+function to json
+"""
 
 class Student:
+"""create class"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
