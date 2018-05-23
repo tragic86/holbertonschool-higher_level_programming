60514ce12f308c01adf41cf6dfc4db623be4e1f8
tragic86 <oneil5@aol.com>
2018-05-23 06:13:05 +0000

    update comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-
+"""Rectangle class"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
-
+    """
+    Args:
+    width
+    height
+    y
+    x
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initilzed"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,10 +22,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -28,10 +37,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -41,10 +52,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -54,10 +67,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """getter"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -66,9 +81,11 @@ class Rectangle(Base):
             self.__y = value
 
     def area(self):
+        """get area"""
         return (self.width * self.height)
 
     def display(self):
+        """print instance"""
         print('\n' * self.y, end="")
         for i in range(self.height):
             print(" " * self.x, end="")
@@ -83,6 +100,7 @@ class Rectangle(Base):
                format(self.id, self.x, self.y, self.width, self.height))
 
     def update(self, *args, **kwargs):
+        """update args and kwargs"""
         new_list = ['id', 'width', 'height', 'x', 'y']
         if kwargs is not None:
             for key, value in kwargs.items():
@@ -91,6 +109,7 @@ class Rectangle(Base):
             setattr(self, new_list[count], element)
 
     def to_dictionary(self):
+        """get dict"""
         a_dict = {
             'id': self.id,
             'width': self.width,
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,22 +10,27 @@ class Square(Rectangle):
     """class defines Square
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """init with superclass"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """str func"""
         return ('[Square] ({:d}) {:d}/{:d} - {:d}'.format(
             self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
+        """getter for size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """size setter"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """make list and update"""
         new_list = ['id', 'size', 'x', 'y']
         if kwargs is not None:
             for key, value in kwargs.items():
@@ -34,6 +39,7 @@ class Square(Rectangle):
             setattr(self, new_list[count], element)
 
     def to_dictionary(self):
+        """new dict"""
         a_dict = {
             'id': self.id,
             'size': self.size,
