939edc5e29926e85d11177fbfee7c2925fe847ad
tragic86 <oneil5@aol.com>
2018-06-25 20:34:46 +0000

    changed to uppercase

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ name VARCHAR(256), score INT);
 INSERT INTO second_table 
        (id, name, score) 
 VALUES 
-       (1, "john", 10),
+       (1, "John", 10),
        (2, "Alex", 3),
        (3, "Bob", 14),
        (4, "George", 8);
