3ac1e277458ad24dae2d35a3f699a92eea075f19
tragic86 <oneil5@aol.com>
2018-06-27 02:47:50 +0000

    join with null values

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- script to show null values
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS	NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
