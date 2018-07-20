e7147203b5227ed713f5ff02f8bcfa303874c973
tragic86 <oneil5@aol.com>
2018-07-20 21:46:18 +0000

    filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""get all states that match the argument"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    """subquery"""
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states where states.name\
+                like '{}'ORDER BY id ASC".format(sys.argv[4]))
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
