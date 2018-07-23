bca8a20d2ae17e5cedf8b1f8c5bf1b2391d1c942
tragic86 <oneil5@aol.com>
2018-07-23 16:49:04 +0000

    get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""list first states"""
+
+import MySQLdb
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    state = session.query(state).filter(state.name == sys.argv[4]).first()
+
+    if state:
+        print('{}'.format(state.id))
+    else:
+        print("Not found")
