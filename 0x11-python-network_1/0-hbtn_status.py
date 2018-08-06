23192fbeca778eab1a267ac8faa4cad75a69dce9
tragic86 <oneil5@aol.com>
2018-08-06 22:08:37 +0000

    get status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    code = html.decode('UTF-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- uft8 content: {}".format(code))
