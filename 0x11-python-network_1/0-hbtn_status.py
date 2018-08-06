b747cf363ea7a98063c18e42ca0e3d4335f4dc0d
tragic86 <oneil5@aol.com>
2018-08-06 22:28:58 +0000

    made lowercase

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        code = html.decode('UTF-8')
+        code = html.decode('utf-8')
 
     print("Body response:")
     print("\t- type: {}".format(type(html)))
