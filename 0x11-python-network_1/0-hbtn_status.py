a7d30a1b54ed77ca2ba2ad2881e0328f8a981dd4
tragic86 <oneil5@aol.com>
2018-08-06 22:12:47 +0000

    added if name is main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,8 @@
 """ script to get request"""
 
 import urllib.request
+if __name__ == "__main__":
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     code = html.decode('UTF-8')
