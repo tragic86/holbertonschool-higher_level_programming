d9c12c1e72704336a01569bbc0cf1c7191a8b910
tragic86 <oneil5@aol.com>
2018-08-06 22:57:41 +0000

    response header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script for header"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.info().get('X-Request-Id'))
