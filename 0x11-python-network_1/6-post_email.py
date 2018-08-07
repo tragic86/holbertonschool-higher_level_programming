67f595a0f6ea99bc0e2ecf15f6df06ad16f314d1
tragic86 <oneil5@aol.com>
2018-08-07 01:06:57 +0000

    added s

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ from sys import argv
 
 if __name__ == "__main__":
     values = {'email': argv[2]}
-    temp = request.post(argv[1], data=values)
+    temp = requests.post(argv[1], data=values)
     print(temp.text)
