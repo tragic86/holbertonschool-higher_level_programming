af5c13c4d54d0fbd393643912aff6f5c30abbfa2
tragic86 <oneil5@aol.com>
2018-09-05 22:05:17 +0000

    star wars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request
+  .get('http://swapi.co/api/films/' + process.argv[2],
+    (error, response, text) => {
+      if (error) throw error;
+      console.log(JSON.parse(text).title);
+    });
